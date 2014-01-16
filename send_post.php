a29a4cb20bc1f681f3243ee3fa2a78c34458395a
thrmarketing <thrmarketingpros@gmail.com>
2014-01-16 18:08:36 -0500

    Update send_post.php

--- send_post.php
+++ send_post.php
@@ -3,5 +3,5 @@
 $connect = mysqli_connect("localhost","giftcard_ppdhelp","passhere","giftcard_ppdhelp");
 //Sending form data to sql db.
 mysqli_query($connect,"INSERT INTO networks (name, offer, image, intructions)
-VALUES ('$_POST[post_name]', '$_POST[post_offer]', '$_POST[post_image]', '$_POST[post_intructions]')";
-?>
+VALUES ('$_POST[post_name]', '$_POST[post_offer]', '$_POST[post_image]', '$_POST[post_intructions]')"
+;?>
